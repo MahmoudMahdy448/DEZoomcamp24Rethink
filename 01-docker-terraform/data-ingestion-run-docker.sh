8ed834868a4ce77f6e5f1899e12ab848a48396df
MahmoudMahdy448 <82063772+MahmoudMahdy448@users.noreply.github.com>
2024-02-28 19:05:38 +0000

    Update Docker image name in data-ingestion-run-docker.sh

--- 01-docker-terraform/data-ingestion-run-docker.sh
+++ 01-docker-terraform/data-ingestion-run-docker.sh
@@ -1,12 +1,13 @@
 # this should run by firstly giving a permission: chmod +x data-ingestion-run-docker.sh
 # then run the script: ./data-ingestion-run-docker.sh
 
+source .env
 
 URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
 
 docker run -it \
   --network=${NETWORK} \
-  taxi_ingest:v001 \
+  ingest-data:v001 \
   --user=${USER} \
   --password=${PASSWORD} \
   --host=${HOST} \
