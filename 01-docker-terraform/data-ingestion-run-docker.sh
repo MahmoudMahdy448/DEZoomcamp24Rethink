a01d119a4f178caa5950b4aa3bfc9ffdbb6697ec
MahmoudMahdy448 <82063772+MahmoudMahdy448@users.noreply.github.com>
2024-02-29 13:41:20 +0000

    Remove unnecessary comments and update data ingestion URL

--- 01-docker-terraform/data-ingestion-run-docker.sh
+++ 01-docker-terraform/data-ingestion-run-docker.sh
@@ -1,6 +1,3 @@
-# this should run by firstly giving a permission: chmod +x data-ingestion-run-docker.sh
-# then run the script: ./data-ingestion-run-docker.sh
-
 source .env
 
 URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
